722abcce0c1239f5ef4618f0dc8e6531400c844c
Dong Van Luu <dvluu@starboardasia.com>
2020-07-17 17:50:59 +0700

    update

--- wp-content/themes/shoppingcart/woocommerce.php
+++ wp-content/themes/shoppingcart/woocommerce.php
@@ -53,7 +53,11 @@ if( 'default' == $layout ) { //Settings from customizer
 </div><!-- end .wrap -->
 
 <?php if(is_product()) { ?>
-	<iframe style="width: 100%; height: 100%; overflow:hidden;" src="<?php echo home_url(); ?>/wp-content/themes/shoppingcart/p_detail_landing/tsubuporon_night_pack/index.html" title="Tsubuporon Night Pack"></iframe>
+	<div class="iframe-container">
+		  <!-- <iframe src="<?php echo home_url(); ?>/wp-content/themes/shoppingcart/p_detail_landing/tsubuporon_night_pack.html" title="Tsubuporon Night Pack"></iframe> -->
+		  <iframe src="<?php echo home_url(); ?>/wp-content/themes/shoppingcart/p_detail_landing/<?php echo get_post_meta($product->get_id(), 'landing_page',  true); ?>" title="Tsubuporon Night Pack"></iframe>
+    </div>
+	<!-- <iframe scrolling="no" id="ifr_pdetail" src="<?php echo home_url(); ?>/wp-content/themes/shoppingcart/p_detail_landing/tsubuporon_night_pack.html" title="Tsubuporon Night Pack"></iframe> -->
 <?php } ?>
 
 <?php
